c9b058363a4b8a64cc79edde607d6405c7e8d1cc
aldipramana270804 <aldipramana@student.uir.ac.id>
2025-03-17 16:20:30 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+console.info("Hello Vue");
+
+const hello = ref("Hello sobat coding");
+const userInput = ref("");
+const tes = ref("coba disini tes");
+const warna = ref("yellow");
+const tebal = ref("bold");
+</script>
+
+<template>
+  <h1 class="hello">{{ hello }}</h1>
+  <div v-html="hello"></div>
+  <input v-model="userInput" type="text" placeholder="Masukan Nama Kamu 😁" />
+  <input type="text" v-model="umurKamu" placeholder="Masukan umur kamu 😀" />
+  <div>
+    <p>Umur kamu 😊: {{ umurKamu }}</p>
+    <p v-if="umurKamu < 15">Kamu masih kecik</p>
+    <p v-else-if="umurKamu >= 15 && umurKamu < 50">Kamu dah besarr</p>
+    <p v-else>Kamuu sudah tuaa</p>
+  </div>
+  <p>Nama yang kamu masukan: {{ userInput }}</p>
+  <div>{{ tes }}</div>
+  <button @click="() => (tes = 'Test telah berjaya!!')">Coba tukar</button>
+
+  <div :style="{ color: warna, 'font-weight': tebal }">
+    {{ userInput }}, {{ umurKamu }}
+  </div>
+</template>
